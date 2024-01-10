f2dd1897234eb28f969930cd6ee83687adbd251c
HemanBabuSeervi <138663338+HemanBabuSeervi@users.noreply.github.com>
2024-01-10 19:38:56 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let settings;
 let canvas=document.getElementById("graph");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
+canvas.width=window.innerWidth*2;
+canvas.height=window.innerHeight*2;
+let body=document.getElementsByTagName('body')[0];
+body.scrollLeft=window.innerWidth*0.5;
+body.scrollTop=window.innerHeight*0.5;
+
+
 class Settings{
-    constructor(equation, units, pixelLenRatio, zeroPoint, colorCurve,  colorAxes, colorGrid, colorFont){
+    constructor(equation, units, pixelLenRatio, zeroPoint, mouseX, colorPlot,  colorAxes, colorGrid){
         this.equation=equation;
         this.units=units;
         this.pixelLenRatio = pixelLenRatio ?? [50,50];
         this.zeroPoint = zeroPoint ?? [canvas.width/2, canvas.height/2];
-        this.colorCurve = colorCurve ?? "rgb(255, 112, 80)";
-        this.colorAxes = colorAxes ?? "rgb(0,0,0)";
-        this.colorGrid = colorGrid ?? "rgb(80, 112, 255)";
-        this.colorFont = colorFont ?? "rgb(80, 80, 0)";
+        this.mouseX = mouseX ?? this.units[0];
+        this.colorPlot = colorPlot ?? (settings!=undefined ? settings.colorPlot : undefined) ?? "rgb(255, 30, 0)";
+        this.colorAxes = colorAxes ?? (settings!=undefined ? settings.colorAxes : undefined) ?? "rgb(0,0,0)";
+        this.colorGrid = colorGrid ?? (settings!=undefined ? settings.colorGrid : undefined) ?? "rgb(80, 112, 255)";
     }
 }
 let ctx=canvas.getContext("2d");
@@ -68,44 +73,56 @@ function drawLines(strokeStyle, lineWidth, step, init){
         y-=step[1];
     }
     ctx.stroke();
+    ctx.closePath();
 }
 function numerateAxesPoints(){
     fontSize=25;
+    readabilityCorrection=fontSize*0.6;
     ctx.font = fontSize+"px serif";
-    ctx.fillStyle = settings.colorFont;
+    ctx.fillStyle = settings.colorAxes;
     ctx.textAlign = "center";
 
-    ctx.fillText('0', pointXToPixelX(0)-fontSize, pointYToPixelY(0)+fontSize);
+    ctx.fillText('0',
+        pointXToPixelX(0)-readabilityCorrection,
+        pointYToPixelY(0)+readabilityCorrection );
     let x=settings.units[0];
     while(pointXToPixelX(x)<canvas.width){
-        ctx.fillText(x, pointXToPixelX(x)-fontSize, pointYToPixelY(0)+fontSize);
+        ctx.fillText(x,
+            pointXToPixelX(x)-readabilityCorrection,
+            pointYToPixelY(0)+readabilityCorrection );
         x+=settings.units[0];
     }
     x=-settings.units[0];
     while(pointXToPixelX(x)>0){
-        ctx.fillText(x, pointXToPixelX(x)-fontSize, pointYToPixelY(0)+fontSize);
+        ctx.fillText(x,
+            pointXToPixelX(x)-readabilityCorrection,
+            pointYToPixelY(0)+readabilityCorrection);
         x-=settings.units[0];
     }
 
     let y=settings.units[1];
     while(pointYToPixelY(y)>0){
-        ctx.fillText(y, pointXToPixelX(0)-fontSize, pointYToPixelY(y)+fontSize);
+        ctx.fillText(y,
+            pointXToPixelX(0)-readabilityCorrection,
+            pointYToPixelY(y)+readabilityCorrection);
         y+=settings.units[1];
     }
     y=-settings.units[1];
     while(pointYToPixelY(y)<canvas.height){
-        ctx.fillText(y, pointXToPixelX(0)-fontSize, pointYToPixelY(y)+fontSize);
+        ctx.fillText(y,
+            pointXToPixelX(0)-readabilityCorrection,
+            pointYToPixelY(y)+readabilityCorrection);
         y-=settings.units[1];
     }
 }
 
 function drawGraph(newSettings){
-    settings=newSettings;
+    settings=newSettings ?? settings;
     const baseColor=addAlpha(settings.colorGrid, 0.2);
     const thinLines=addAlpha(settings.colorGrid, 0.5);
     const thickLines=settings.colorGrid;
     const axesColor=settings.colorAxes;
-    const curveColor=settings.colorCurve;
+    const curveColor=settings.colorPlot;
 
     //Reset Canvas to White
     ctx.fillStyle="white";
@@ -125,6 +142,7 @@ function drawGraph(newSettings){
     ctx.moveTo(settings.zeroPoint[0], 0);
     ctx.lineTo(settings.zeroPoint[0], canvas.height);
     ctx.stroke();
+    ctx.closePath();
 
 
     //Drawing ThinLines;
@@ -141,13 +159,39 @@ function drawGraph(newSettings){
     ctx.beginPath();
     let pixelX=0;
     const f = math.parse(settings.equation);
+    if(f==null || f==undefined) alert("Invalid Function.");
     while(pixelX<canvas.width){
-        let pixelY = pointYToPixelY(f.evaluate( { x : pixelXToPointX(pixelX) } ) );
+        let y=f.evaluate( { x : pixelXToPointX(pixelX) } );
+        let pixelY;
+        pixelY = pointYToPixelY(y);
         if(pixelX==0) ctx.moveTo(0, pixelY);
         else ctx.lineTo(pixelX, pixelY);
         pixelX+=1;
     }
     ctx.stroke();
+    ctx.closePath();
+
+    //MouseHoverValues
+    ctx.beginPath();
+    ctx.arc(
+        pointXToPixelX(settings.mouseX),
+        pointYToPixelY(f.evaluate( { x : settings.mouseX })),
+        5,
+        0,
+        2*Math.PI
+    );
+    ctx.fillStyle=curveColor;
+    ctx.fill();
+    ctx.closePath();
+    let x=settings.mouseX.toFixed(2);
+    let X=pointXToPixelX(x);
+    let y=f.evaluate({x : x}).toFixed(2);
+    let Y=pointYToPixelY(y);
+    let offset=6;
+    ctx.fontSize=16;
+    ctx.textAlign='left';
+    ctx.fillText('('+x+','+y+')',X+offset,Y+offset);
+
 }
 function drawGraphDOM(){
     const equation=document.getElementById("eq").value;
@@ -170,4 +214,61 @@ function reScaleDOM(){
     );
     drawGraph(newSettings);
 }
+function evaluateDOM(){
+    const x=Number(document.getElementById('ev').value);
+    const evResultDOM=document.getElementById('ev-result');
+    evResultDOM.innerHTML=math.parse(settings.equation).evaluate( { x : x } ).toFixed(3);
+}
+function toggleOptionsVisible(){
+    let options=document.getElementById('options');
+    let gear=document.querySelector('#settings>button');
+    if(options.style.display=="none" || options.style.display==""){
+        options.style.display="block";
+        gear.style.display="none";
+    } else {
+        options.style.display="none";
+        gear.style.display="inline";
+    }
+}
+function themeChange(){
+    canvas.width=sheetWidthDOM.value;
+    canvas.height=sheetHeightDOM.value;
+    drawGraph(
+        new Settings( settings.equation,
+                    settings.units,
+                    settings.pixelLenRatio,
+                    [canvas.width/2, canvas.height/2],
+                    settings.mouseX,
+                    plotColorDOM.value,
+                    axesColorDOM.value,
+                    sheetColorDOM.value
+        )
+    );
+    toggleOptionsVisible();
+}
 drawGraph( new Settings("x*sin(x)", [1,1] ,[40,40]));
+
+//Populating settings Panel
+let sheetWidthDOM=document.querySelector("#w");
+let sheetHeightDOM=document.querySelector("#h");
+let sheetColorDOM=document.querySelector("#sc");
+let axesColorDOM=document.querySelector("#ac");
+let plotColorDOM=document.querySelector("#pc");
+sheetWidthDOM.value=canvas.width;
+sheetHeightDOM.value=canvas.height;
+sheetColorDOM.value=settings.colorGrid;
+axesColorDOM.value=settings.colorAxes;
+plotColorDOM.value=settings.colorPlot;
+
+
+canvas.addEventListener('mousemove', (event)=>{
+    drawGraph(
+        new Settings(
+            settings.equation,
+            settings.units,
+            settings.pixelLenRatio,
+            settings.zeroPoint,
+            pixelXToPointX(event.clientX-canvas.getBoundingClientRect().left)
+        )
+    );
+});
